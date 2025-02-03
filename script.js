3ea90cea97d8b34cec9074c43b79c77e1e33d2eb
lensclear <lensclear@outlook.com>
2025-02-03 10:44:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        uploadedFiles = validFiles.slice(0, 20); // Limit to 20 files
+        uploadedFiles = validFiles;
         
         // Show UI elements
         previewArea.classList.remove('hidden');
